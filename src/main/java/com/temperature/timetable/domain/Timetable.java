62297c7902c459d8af936f34d197131e3e06596b
laishengkai20050204 <48379994+laishengkai20050204@users.noreply.github.com>
2026-08-26 20:05:08 +0800

    feat: add timetable planning solution

---
+++ src/main/java/com/temperature/timetable/domain/Timetable.java
@@ -0,0 +1,58 @@
+package com.temperature.timetable.domain;
+
+import java.util.List;
+
+import ai.timefold.solver.core.api.domain.solution.PlanningEntityCollectionProperty;
+import ai.timefold.solver.core.api.domain.solution.PlanningScore;
+import ai.timefold.solver.core.api.domain.solution.PlanningSolution;
+import ai.timefold.solver.core.api.domain.solution.ProblemFactCollectionProperty;
+import ai.timefold.solver.core.api.domain.valuerange.ValueRangeProvider;
+import ai.timefold.solver.core.api.score.buildin.hardsoft.HardSoftScore;
+
+@PlanningSolution
+public class Timetable {
+
+    @ProblemFactCollectionProperty
+    @ValueRangeProvider(id = "timeslotRange")
+    private List<Timeslot> timeslots;
+
+    @ProblemFactCollectionProperty
+    private List<TeacherUnavailable> teacherUnavailableList;
+
+    @PlanningEntityCollectionProperty
+    private List<Lesson> lessons;
+
+    @PlanningScore
+    private HardSoftScore score;
+
+    public Timetable() {
+    }
+
+    public Timetable(List<Timeslot> timeslots,
+                     List<TeacherUnavailable> teacherUnavailableList,
+                     List<Lesson> lessons) {
+        this.timeslots = timeslots;
+        this.teacherUnavailableList = teacherUnavailableList;
+        this.lessons = lessons;
+    }
+
+    public List<Timeslot> getTimeslots() {
+        return timeslots;
+    }
+
+    public List<TeacherUnavailable> getTeacherUnavailableList() {
+        return teacherUnavailableList;
+    }
+
+    public List<Lesson> getLessons() {
+        return lessons;
+    }
+
+    public HardSoftScore getScore() {
+        return score;
+    }
+
+    public void setScore(HardSoftScore score) {
+        this.score = score;
+    }
+}
